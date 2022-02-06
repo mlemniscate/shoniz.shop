844f4d31e3d43286644d361418bafbc849867517
4000665 <4000665@JAMALIVAND-ZH>
2022-02-06 12:07:44 +0330

    Add project files.

---
+++ shoniz.shop.CustomerContext.Domain/Customers/Customer.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace shoniz.shop.CustomerContext.Domain.Customers
+{
+    class Customer
+    {
+        public Customer(string nationalCode, string email, string password, string firstName, string lastName)
+        {
+            SetNationalCode(nationalCode);
+            SetEmail(email);
+            SetPassword(password);
+            SetName(firstName, lastName);
+        }
+
+        
+
+        public string NationalCode { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        private void SetNationalCode(string nationalCode)
+        {
+            if (string.IsNullOrWhiteSpace(nationalCode))
+            {
+                throw new Exceptions.NationalCodeRequiredException();
+            }
+
+            if (nationalCode.Length < 10)
+            {
+
+            }
+
+            if(nationalCode.All(c=> !char.IsDigit(c)))
+            {
+
+            }
+
+            NationalCode = nationalCode;
+        }
+
+        private void SetEmail(string email)
+        {
+            if(string.IsNullOrWhiteSpace(email))
+            {
+
+            }
+            if (!Regex.IsMatch(email, ""))
+            {
+
+            }
+
+            Email = email;
+        }
+
+        private void SetName(string firstName, string lastName)
+        {
+            //validate 
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        private void SetPassword(string password)
+        {
+            //validate
+            Password = password;
+        }
+    }
+}
---
+++ shoniz.shop.CustomerContext.Domain/Customers/Exceptions/NationalCodeRequiredException.cs
@@ -0,0 +1,14 @@
+﻿using shoniz.Framework.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shoniz.shop.CustomerContext.Domain.Customers.Exceptions
+{
+    class NationalCodeRequiredException : DomainException
+    {
+        public override string Message => Resources.ExceptionResource.NationalCodeRequiredException;
+    }
+}
