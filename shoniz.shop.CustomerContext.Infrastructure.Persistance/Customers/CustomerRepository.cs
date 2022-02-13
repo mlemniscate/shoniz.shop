45bc48dfda97cadbadb98bf78b70522751c96f16
Milad <miladabdi1374@gmail.com>
2022-02-13 13:16:00 +0330

    put some implementation with dbContext in CustomerRepository.

--- shoniz.shop.CustomerContext.Infrastructure.Persistance/Customers/CustomerRepository.cs
+++ shoniz.shop.CustomerContext.Infrastructure.Persistance/Customers/CustomerRepository.cs
@@ -1,7 +1,9 @@
 ﻿using shoniz.Framework.Persistance;
 using shoniz.shop.CustomerContext.Domain.Customers;
 using shoniz.shop.CustomerContext.Domain.Customers.Services;
+using System.Data.Entity;
 using System;
+using System.Linq;
 using System.Linq.Expressions;
 
 namespace shoniz.shop.CustomerContext.Infrastructure.Persistance.Customers
@@ -16,7 +18,7 @@ namespace shoniz.shop.CustomerContext.Infrastructure.Persistance.Customers
         }
         public bool Contains(Expression<Func<Customer, bool>> predicate)
         {
-            dbContext.SaveChanges();
+            return dbContext.Set<Customer>().Any(predicate);
         }
 
         public void CreateCustomer(Customer customer)
@@ -26,6 +28,10 @@ namespace shoniz.shop.CustomerContext.Infrastructure.Persistance.Customers
 
         public Customer GetCustomer(Guid customerId)
         {
+            return dbContext
+                .Set<Customer>()
+                .Include(c => c.Addresses)
+                .Single(c => c.Id == customerId);
         }
 
         public void Update(Customer customer)
